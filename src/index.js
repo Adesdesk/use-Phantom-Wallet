8c8243e9f7bfc0a804fef2bfe98baa12b73be2a3
Adeola <aadelakun28@gmail.com>
2023-07-13 20:34:15 +0100

    Adding alternative tsx file.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import SolanaButton from "./App";
+// import SolanaButton from "./App";
+import App from "./AppII.tsx";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SolanaButton />
+    {/* <SolanaButton /> */}
+    <App />
   </React.StrictMode>
 );
 
